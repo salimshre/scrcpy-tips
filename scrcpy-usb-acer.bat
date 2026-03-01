30227e9cab2b2d0370a2697e30095b34962273ee
Salim Shrestha <salim9shrestha@gmail.com>
2026-03-01 22:39:22 +0545

    update

---
+++ scrcpy-usb-acer.bat
@@ -0,0 +1,15 @@
+@echo off
+cd /d C:\Users\ACER-WIN10\Documents\GitHub\scrcpy-tips\scrcpy-win64-v3.3.1
+
+:: Kill adb and restart fresh
+adb kill-server
+adb start-server
+
+:: Disconnect any Wi-Fi devices to avoid conflicts
+adb disconnect
+
+:: Show connected devices (should only show USB)
+adb devices
+
+:: Start scrcpy forcing USB mode
+scrcpy -d --max-size 1024
